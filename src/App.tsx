d3b6499a5e194bbe9a94e660f9f016e2b3950014
minyoung2 <minyoung2@nts-corp.com>
2022-01-13 23:20:21 +0900

    [TypeScript] Typing the Props

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Circle from "./Component/Circle";
+
+function App(){
+  return (
+    <div>
+      <Circle bgColor="teal"/>
+      <Circle bgColor="orange" />
+    </div>
+  );
+}
+export default App;
