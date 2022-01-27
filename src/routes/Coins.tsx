255a15b3372850a4ac462eda7bd40b1a5bd4dc27
minyoung2 <minyoung2@nts-corp.com>
2022-01-27 23:51:31 +0900

    [CRYPTO TRACKER] Route state

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -24,9 +24,10 @@ const Coin = styled.li`
   margin-bottom: 10px;
 
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -43,6 +44,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 25px;
+  height:25px;
+  margin-right: 10px;
 `;
 
 interface CoinInerface {
@@ -77,7 +84,17 @@ function Coins(){
                 <CoinsList>
                     {coins.map(coin => 
                         <Coin key={coin.id}>
-                            <Link to={coin.id}>{coin.name} &rarr;</Link>
+                            <Link
+                              to={{
+                                pathname: `/${coin.id}`,
+                                state: { name: coin.name },
+                              }}
+                            >
+                              <Img
+                                src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                              />
+                              {coin.name} &rarr;
+                              </Link>
                         </Coin>
                     )}
                 </CoinsList>
